2d51317e6ffc4fe6a14a44a7aba171f035dcf81b
DylanMorison <dylan@g.morison.org>
2022-12-15 18:24:49 -0800

    init

---
+++ src/main/java/com/dylanmorison/spring5webapp/Spring5webappApplication.java
@@ -0,0 +1,16 @@
+package com.dylanmorison.spring5webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5webappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5webappApplication.class, args);
+		System.out.println("hello world!");
+	}
+
+	
+
+}
